9fc8e2b462e8c5274b429df7cb99416a7cf906bb
amor20130030328 <1770944692@qq.com>
2019-01-19 18:41:01 +0800

    提交组织服务代码

--- organization.java
+++ organization.java
@@ -1,28 +1,7 @@
 public class Organization{
 
-  private String name;
-  private String code;   //edit by fan
-  private int age;    //edit by amor
-  
-  
-   public String getName(){
-      return name;
-   }
+ 
 	
-   public void setName(String name){
-   
-   	this.name = name;
-   
-   }
-
-   public String getCode(){
-   	return code;
-   }
-	
-   public void setAge( int age){
-
-	this.age = age;
-   }//
-   
+ 
 
 }
